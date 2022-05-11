ab0c84c85fb0d78f532c8263cdc6ffc7b84c8efa
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-05-11 17:55:35 +0400

    использовать контекст в Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
 function Card({item, onCardClick}) {
+    const currentUser = React.useContext(CurrentUserContext);
+    // Определяем, являемся ли мы владельцем текущей карточки
+    const isOwn = item.owner._id === currentUser._id;
+
+    // Создаём переменную, которую после зададим в `className` для кнопки удаления
+    const cardDeleteButtonClassName = (
+        `card__delete-icon ${isOwn ? '' : 'card__delete-icon_hidden'}`
+    );
+
+    // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+    const isLiked = item.likes.some(i => i._id === currentUser._id);
+
+    // Создаём переменную, которую после зададим в `className` для кнопки лайка
+    const cardLikeButtonClassName = (
+        `card__like-icon ${isLiked ? 'card__like-icon_active' : ''}`
+    );
+
     const handleCardClick = () => {
         onCardClick(item);
     };
     return(
             <article className="card">
                 <div className="card__container">
-                    <button className="card__delete-icon" type="button" aria-label="Удалить" />
+                    <button className={cardDeleteButtonClassName} type="button" aria-label="Удалить" />
                     <img className="card__image" src={item.link} alt={item.name}
                          onClick={handleCardClick}
                     />
@@ -15,7 +33,7 @@ function Card({item, onCardClick}) {
                 <div className="card__caption">
                     <h2 className="card__place-title">{item.name}</h2>
                     <div className="card__like">
-                        <button className="card__like-icon" type="button" aria-label="Нравится" />
+                        <button className={cardLikeButtonClassName} type="button" aria-label="Нравится" />
                         <p className="card__like-amount">{item.likes.length}</p>
                     </div>
                 </div>
